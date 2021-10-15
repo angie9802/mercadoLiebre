f79e4a538f418bc8b860a59e29928b08202ae341
angie9802 <avbarajasc@unal.edu.co>
2021-10-14 23:45:59 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
+
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+})
+
+app.use(express.static(path.resolve(__dirname,'./public')))
+
+app.listen(port, ()=>{
+    console.log("Running server in port "+port)
+})
