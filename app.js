276728bfd128eba5079f1a24a2d288a7b60f2fb0
Angie Cantor <avbarajasc@unal.edu.co>
2021-10-15 23:56:47 -0500

    login

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 3000
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 })
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
 
 app.use(express.static(path.resolve(__dirname,'./public')))
 
